92087f7e2d324fd27bc5a754d89c776788ceee50
Wrulf <129190080+Wrulf@users.noreply.github.com>
2023-06-08 11:53:27 +0000

    Created biketrip model

---
+++ db/models/bikestation.js
@@ -0,0 +1,3 @@
+/**
+ * Description: exports a model for of bikestations enabling the app to interact with the data in the database.
+ */
--- db/models/biketrip.js
+++ db/models/biketrip.js
@@ -3,6 +3,68 @@
  */
 
 
-// require sequelize
+const Sequelize = require("sequelize"); // Import sequelize orm module
 
-// export defined placeholder that can be filled with specified data.
+// export placeholder function for model definition
+module.exports = (sequelize, DataTypes) => {
+    class Biketrip extends Sequelize.model {}   // create subclass 
+    
+    Biketrip.init(
+    // first object field definitions    
+    {
+        // An autoincrementin ID is left out due to an assumption of performance optimization
+        departureTime: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            field: "Departure",
+          },
+          returnTime: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            field: "Return"
+          },
+          departureStationId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: "Departure station id",
+          }, 
+        // Ommited due to getting the same data from departureStationId + Bikestations table  
+        //   departureStationName: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //   },
+          returnStationId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: "Return station id",
+          },
+        // Ommited due to getting the same data from departureStationId + Bikestations table 
+        //   returnStationName: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //   },
+          coveredDistanceMeters: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: "Covered distance (m)",
+            validate: {
+                // (no distance < 10) handled in csv-parser script 
+            },
+          },
+          durationSeconds: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: "Duration (sec.)",
+            validate: {
+                // (no duration < 10) handled in csv-parser script
+            },
+          },
+    },
+    // second object to configure the model
+    {
+        sequelize
+    }
+    );
+
+    return Biketrip
+};
