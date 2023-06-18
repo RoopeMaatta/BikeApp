de774d177c63e7893662c5990df132175df110f7
Wrulf <129190080+Wrulf@users.noreply.github.com>
2023-06-18 14:50:34 +0000

    Generalize suggestions and rework comments

--- public/suggestions.js
+++ public/suggestions.js
@@ -1,107 +1,93 @@
 /**
- * Description: Suggests station names on input
+ * createSuggestions creates an autocomplete input field with a list of suggestions.
+ *
+ * @param {string} inputElementId - The ID of the input field to add autocomplete functionality.
+ * @param {string} suggestionsUrl - The URL of the API endpoint to fetch suggestions.
+ * @param {string} selectedContainerId - The ID of the element where selected items from the autocomplete will be added.
+ *
+ * @returns {void}
  */
 
 
-function getSelectedStations() {
-  const tags = document.querySelectorAll('#selected-stations .tag');
-  const selectedStations = Array.from(tags).map(tag => ({
-    name: tag.childNodes[0].textContent,  // get textContent of the first child node (station name)
-    id: tag.dataset.id  // get the data-id attribute of the tag (station ID)
-  }));
-  return selectedStations;
+function createSuggestions(inputElementId, suggestionsUrl, selectedContainerId) {
+  let timer;  // Timer to delay suggestion fetching
+  const inputElement = document.getElementById(inputElementId);  // Input field
+  const selectedContainer = document.getElementById(selectedContainerId);  // Container for selected items
+  let lastSuggestions = [];  // Array to store the latest suggestions
+
+  // Event listener for when the user types in the input field
+  inputElement.addEventListener('input', (event) => {
+    // Clear any previous timeout
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    // Delay suggestion fetching until the user stops typing
+    timer = setTimeout(async () => {
+      // Clear out the old suggestions
+      const dataList = inputElement.list;
+      dataList.innerHTML = '';
+
+      // Create URL with user input as query parameter
+      let url = new URL(suggestionsUrl, window.location.origin);
+      const currentInputValue = inputElement.value.trim();
+      let params = {
+        q: currentInputValue
+      };
+      for (let key in params) {
+        url.searchParams.append(key, params[key]);
+      }
+
+      // Fetch the suggestions from the API
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const suggestions = await response.json();
+
+      // Store the latest suggestions
+      lastSuggestions = suggestions;
+
+      // Populate the datalist with the new suggestions
+      for (const suggestion of suggestions) {
+        const option = document.createElement('option');
+        option.value = suggestion.name;
+        option.dataset.id = suggestion.id;
+        dataList.appendChild(option);
+      }
+    }, 0);
+  });
+
+  // Event listener for when the user selects a suggestion
+  inputElement.addEventListener('input', () => {
+    const currentValue = inputElement.value.trim();
+    // If the current value is in the list of suggestions, add it to the selected items
+    if (currentValue !== '' && lastSuggestions.some(suggestion => suggestion.name === currentValue)) {
+      const matchingSuggestion = lastSuggestions.find(suggestion => suggestion.name === currentValue);
+
+      // Create a new tag for the selected item and append it to the selected container
+      const tag = document.createElement('span');
+      tag.textContent = currentValue;
+      tag.classList.add('tag');
+      tag.dataset.id = matchingSuggestion.id;
+
+      // Add a remove button to the tag
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'x';
+      removeButton.classList.add('remove-button');
+      tag.appendChild(removeButton);
+
+      selectedContainer.appendChild(tag);
+      inputElement.value = '';
+    }
+  });
+
+  // Event listener for when the user clicks on the remove button of a selected item
+  selectedContainer.addEventListener('click', (event) => {
+    if (event.target.matches('.remove-button')) {
+      event.target.parentNode.remove();
+    }
+  });
 }
-
-
-// Usage
-//console.log(getSelectedStations());  // Will output an array of selected station names
-
-
-
-
-
-// Populate datalist with suggestions fetch
-let timer;
-const stationInput = document.getElementById('station-input');
-const selectedStationsContainer = document.getElementById('selected-stations');
-let lastSuggestions = [];  // NEW: variable to store the latest list of suggestions
-
-stationInput.addEventListener('input', (event) => {
-  // Clear the previous timeout if it exists
-  if (timer) {
-    clearTimeout(timer);
-  }
-
-  // Set a new timeout
-  timer = setTimeout(async () => {
-    // Clear out the old options
-    const dataList = document.getElementById('stations');
-    dataList.innerHTML = '';
-
-    // Create a new URL object
-    let url = new URL('/api/suggestions', window.location.origin);
-
-    // Gather user input into params object
-    const currentInputValue = stationInput.value.trim();
-
-    let params = {
-      q: currentInputValue
-    };
-
-    // Add each parameter to the URL
-    for (let key in params) {
-      url.searchParams.append(key, params[key]);
-    }
-
-    // Make the HTTP request
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const stationNames = await response.json();
-
-    // NEW: update lastSuggestions
-    lastSuggestions = stationNames;
-
-    // Populate the datalist with the new options
-    for (const station of stationNames) {
-      const option = document.createElement('option');
-      option.value = station.name;  // Set the visible value to the station's name
-      option.dataset.id = station.id;  // Store the station's ID in a data attribute
-      dataList.appendChild(option);
-    }
-  }, 0); // Fetch suggestions x ms after the user stops typing
-});
-
-
-// If input field matches suggestion field add to tag
-// not optimal but workaround with datalist.
-// NEW: Listen for changes to the input field
-stationInput.addEventListener('input', () => {
-  const currentValue = stationInput.value.trim();
-  // add the current value to the selected stations, if it's in the list of last suggestions
-  if (currentValue !== '' && lastSuggestions.some(station => station.name === currentValue)) {
-    const matchingStation = lastSuggestions.find(station => station.name === currentValue);  // Find the matching station
-
-    const tag = document.createElement('span');
-    tag.textContent = currentValue;
-    tag.classList.add('tag');
-    tag.dataset.id = matchingStation.id;  // Store the station's ID in a data attribute
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'x';
-    removeButton.classList.add('remove-button');
-    tag.appendChild(removeButton);
-
-    selectedStationsContainer.appendChild(tag);
-    stationInput.value = '';
-  }
-});
-
-selectedStationsContainer.addEventListener('click', (event) => {
-  if (event.target.matches('.remove-button')) {
-    event.target.parentNode.remove();
-  }
-});
