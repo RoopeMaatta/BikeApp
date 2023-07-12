9c25d68ed26a16fc1654029879145c1bfd27a0aa
Wrulf <129190080+Wrulf@users.noreply.github.com>
2023-07-12 09:18:55 +0000

    Improve error handling & logging in trips importer

--- scripts/csvToDatabaseTrips.js
+++ scripts/csvToDatabaseTrips.js
@@ -10,21 +10,21 @@ const BATCH_SIZE = 50;
 
 const importData = async () => {
   const logStream = fs.createWriteStream(path.join(__dirname, 'unimportedTrips.log'), { flags: 'a' });
-
+  
   // Get all bike station IDs for foreign key check
   const bikeStationRecords = await db.models.Bikestation.findAll({ attributes: ['ID'] });
   const bikeStationIds = bikeStationRecords.map(station => station.ID);
   
-
-
+  
+  
   const seenRows = new Set();  // Keep track of seen rows for checking duplicate entries
-
+  
   const isCoveredDistanceOrDurationTooSmall = (row) => {
     const coveredDistanceMeters = parseInt(row.coveredDistanceMeters);
     const durationSeconds = parseInt(row.durationSeconds);
     return coveredDistanceMeters <= 10 || durationSeconds <= 10;
   };
-
+  
   const isDuplicateFields = (row) => {
     const rowString = JSON.stringify(row);
     if (seenRows.has(rowString)) {
@@ -34,25 +34,25 @@ const importData = async () => {
       return false;
     }
   };
-
+  
   const isForeignKeyInvalid = (row) => {
     return !bikeStationIds.includes(row.departureStationId) || !bikeStationIds.includes(row.returnStationId);
   };
-
+  
   try {
     const csvFiles = fs.readdirSync(dataFolder).filter(file => file.endsWith('.csv'));
     for (const file of csvFiles) {
-      if (file === 'bikeStations.csv' || file === "testTrips.csv" || file === "testTrips2.csv" || file === "2021-05xxx.csv") {
+      if (file === 'bikeStations.csv' || file === "testTrips.csv" || file === "testTrips2xxx.csv" || file === "2021-05.csv") {
         console.log(`Skipping file: ${file}`);
         continue;
       }
-
+      
       const filePath = path.join(dataFolder, file);
-
+      
       await new Promise((resolve, reject) => {
         let isInitialChunk = true;
         let rowsBatch = [];
-
+        
         const removeBomTransform = new stream.Transform({
           transform(chunk, encoding, callback) {
             if (isInitialChunk) {
@@ -64,67 +64,140 @@ const importData = async () => {
             callback(null, chunk);
           }
         });
-
+        
         const csvStream = fs.createReadStream(filePath)
-          .pipe(removeBomTransform)
-          .pipe(csv({ separator: ',' }));
-
+        .pipe(removeBomTransform)
+        .pipe(csv({ separator: ',' }));
+        
         csvStream
-          .on('data', (dataRow) => {
-            const row = {
-              departureTime: new Date(dataRow['Departure']),
-              returnTime: new Date(dataRow['Return']),
-              departureStationId: parseInt(dataRow['Departure station id']),
-              returnStationId: parseInt(dataRow['Return station id']),
-              coveredDistanceMeters: parseInt(dataRow['Covered distance (m)']),
-              durationSeconds: parseInt(dataRow['Duration (sec.)']),
-            };
-
-            let errorCategory = null;
-            if (isCoveredDistanceOrDurationTooSmall(row)) {
-              errorCategory = 'Distance or Duration < 10';
-            } else if (isDuplicateFields(row)) {
-              errorCategory = 'Trip already in database';
-            } else if (isForeignKeyInvalid(row)) {
-              errorCategory = 'Depart- or Returnstation ID not in Bikestations ID-list';
+        .on('data', (dataRow) => {
+          // const row = {
+          //   departureTime: new Date(dataRow['Departure']),
+          //   returnTime: new Date(dataRow['Return']),
+          //   departureStationId: parseInt(dataRow['Departure station id']),
+          //   returnStationId: parseInt(dataRow['Return station id']),
+          //   coveredDistanceMeters: parseInt(dataRow['Covered distance (m)']),
+          //   durationSeconds: parseInt(dataRow['Duration (sec.)']),
+          // };
+          const row = {
+            departureTime: new Date(dataRow['Departure']),
+            returnTime: new Date(dataRow['Return']),
+            departureStationId: Number(dataRow['Departure station id']),
+            returnStationId: Number(dataRow['Return station id']),
+            coveredDistanceMeters: Number(dataRow['Covered distance (m)']),
+            durationSeconds: Number(dataRow['Duration (sec.)']),
+          };
+          
+          
+          // check for invalid Date
+          if (row.departureTime.toString() === 'Invalid Date' || row.returnTime.toString() === 'Invalid Date') {
+            logStream.write(`Invalid date: ${JSON.stringify(row)}\n`);
+            return;
+          }
+          
+          
+          // Check for missing data
+          const isDataMissing = (row) => {
+            const keys = ['departureTime', 'returnTime', 'departureStationId', 'returnStationId', 'coveredDistanceMeters', 'durationSeconds'];
+            for (const key of keys) {
+              if (row[key] === undefined) return true;
             }
-
-            if (errorCategory) {
-              logStream.write(`${errorCategory}: ${JSON.stringify(row)}\n`);
-            } else {
-              rowsBatch.push(row);
-            }
-
-            if (rowsBatch.length === BATCH_SIZE) {
-              csvStream.pause(); // pause reading until insertion is finished
-
-              db.models.Biketrip.bulkCreate(rowsBatch, { ignoreDuplicates: true })
-                .then(() => {
-                  rowsBatch = [];
-                  csvStream.resume(); // resume reading once insertion is finished
-                })
-                .catch(err => {
-                  console.error(err);
-                  logStream.write(`other error: ${JSON.stringify(rowsBatch)}\n`);
-                  rowsBatch = [];
-                  csvStream.resume(); // resume reading even if there was an error
-                });
-            }
-          })
-          .on('end', async () => {
-            // Handle remaining rows in the batch
-            if (rowsBatch.length > 0) {
-              try {
-                await db.models.Biketrip.bulkCreate(rowsBatch, { ignoreDuplicates: true });
-              } catch (err) {
-                console.error(err);
-                logStream.write(`other error: ${JSON.stringify(rowsBatch)}\n`);
+            return false;
+          };
+          
+          // Check for extra or missing data
+          const hasIncorrectColumnCount = (dataRow) => {
+            const expectedColumns = ['Departure', 'Return', 'Departure station id', 'Departure station name', 'Return station id', 'Return station name', 'Covered distance (m)', 'Duration (sec.)'];
+            const dataRowColumns = Object.keys(dataRow);
+            // Check if dataRow has any columns not included in the expected columns
+            for (const column of dataRowColumns) {
+              if (!expectedColumns.includes(column)) {
+                return true;
               }
-              rowsBatch = [];
             }
-            resolve();
-          })
-          .on('error', reject);
+            // Check if dataRow has fewer columns than expected
+            if (dataRowColumns.length !== expectedColumns.length) {
+              return true;
+            }
+          
+            return false;
+          };
+          
+          
+          // Check for faulty data
+          const isFaultyData = (row) => {
+            const numericKeys = ['departureStationId', 'returnStationId', 'coveredDistanceMeters', 'durationSeconds'];
+            for (const key of numericKeys) {
+              if (isNaN(parseFloat(row[key])) || parseFloat(row[key]) !== parseInt(row[key]) || isNaN(Number(row[key]))) return true;
+            }
+            return false;
+          };
+          
+          
+          errorCategory = null;
+          let isValidRow = true;
+          if (hasIncorrectColumnCount(dataRow)) {
+            errorCategory = 'Extra or missing column data';
+            isValidRow = false;
+          } else if (isCoveredDistanceOrDurationTooSmall(row)) {
+            errorCategory = 'Distance or Duration < 10';
+            isValidRow = false;
+          } else if (isDuplicateFields(row)) {
+            errorCategory = 'Trip already in database';
+            isValidRow = false;
+          } else if (isForeignKeyInvalid(row)) {
+            errorCategory = 'Depart- or Returnstation ID not in Bikestations ID-list';
+            isValidRow = false;
+          } else if (isDataMissing(row)) {
+            errorCategory = 'Missing column data';
+            isValidRow = false;
+          } else if (isFaultyData(row)) {
+            errorCategory = 'Faulty column data';
+            isValidRow = false;
+          }
+          
+          if (isValidRow) {
+            rowsBatch.push(row);
+          } else if (errorCategory) {
+            logStream.write(`${errorCategory}: ${JSON.stringify(row)}\n`);
+          } else {
+            logStream.write(`Unknown error: ${JSON.stringify(row)}\n`);
+          }
+          
+          
+          
+          
+          
+          if (rowsBatch.length === BATCH_SIZE) {
+            csvStream.pause(); // pause reading until insertion is finished
+            
+            db.models.Biketrip.bulkCreate(rowsBatch, { ignoreDuplicates: true })
+            .then(() => {
+              rowsBatch = [];
+              csvStream.resume(); // resume reading once insertion is finished
+            })
+            .catch(err => {
+              console.error(err);
+              logStream.write(`other error: ${JSON.stringify(rowsBatch)}\n`);
+              rowsBatch = [];
+              csvStream.resume(); // resume reading even if there was an error
+            });
+          }
+        })
+        .on('end', async () => {
+          // Handle remaining rows in the batch
+          if (rowsBatch.length > 0) {
+            try {
+              await db.models.Biketrip.bulkCreate(rowsBatch, { ignoreDuplicates: true });
+            } catch (err) {
+              console.error(err);
+              logStream.write(`other error: ${JSON.stringify(rowsBatch)}\n`);
+            }
+            rowsBatch = [];
+          }
+          resolve();
+        })
+        .on('error', reject);
       });
     }
   } catch (err) {
--- scripts/tripsTableClear.sh
+++ scripts/tripsTableClear.sh
@@ -3,4 +3,5 @@ database_path="/workspaces/BikeApp/database.db"
 table_name="Biketrips" 
 sqlite3 "$database_path" <<EOF
 DELETE FROM $table_name;
+DELETE FROM sqlite_sequence WHERE name='Biketrips';
 EOF
